3a4248d97c0c20847f8566e7755939693eb515a4
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-06-09 20:32:13 +0300

    added model and migrations

---
+++ database/migrations/2023_06_09_172428_create_customers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 40)->nullable();
+            $table->string('surname', 40)->nullable();
+            $table->string('tel', 15)->nullable();
+            $table->longText('address')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
